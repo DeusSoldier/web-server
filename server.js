ff5e82f75f602678de38205f822e7ca12d8ab3ee
Sainz Romero <hellrm1@gmail.com>
2016-04-14 20:55:26 -0500

    add /todos and /todos/id

--- server.js
+++ server.js
@@ -3,15 +3,41 @@ var app=express();
 var port=3000;
 var middleware=require('./middleware.js');
 
-
+var todos=[{
+	id: 1,
+	description: 'dfdsadghhhh',
+	completed: false
+}, {
+	id: 2,
+	description: 'fdsfsdf',
+	completed: false
+}]
 
 app.use(middleware.logger);
 
+app.get('/todos',function(req,res){
+	res.json(todos);
+});
 
+app.get('/todos/:id',function(req,res){
+	var todoid = parseInt(req.params.id,10);
+	var match;
+
+	todos.forEach(function(todo){
+		if(todo.id===todoid)
+			match = todo;
+	});
+		if(match)
+			res.json(match);
+		else
+			res.send('couldnt find todo whit id '+ todoid);
+	
+});
 
 app.get('/about',middleware.requireAuthentification,function(req,res){
+
 	res.send('about');
-})
+});
 
 app.use(express.static(__dirname+'/public'));
 
